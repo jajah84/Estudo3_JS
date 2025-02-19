2ae4eac0b3e8f6718ac7d968694a0383004fecbc
Jailton Pereira <jailton.psj@hotmail.com>
2025-02-19 19:25:14 -0300

    Melhorias

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,26 +1,57 @@
-let nomeAmigo = document.getElementById("nome-amigo"); 
-let agrupaAmigo = [];
-let listaAmigos = document.getElementById("lista-amigos");
-let listaSorteio = document.getElementById("lista-sorteio");
+let amigos = [];
 
-function adicionar(){
-    agrupaAmigo.push(nomeAmigo.value);
-    document.getElementById("nome-amigo").value='';
-    listaAmigos.innerHTML = agrupaAmigo;
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo');
+    let lista = document.getElementById('lista-amigos');
 
-}
-
-function sortear(){
-
-for (let i = 0; i < listaAmigos.length; i++) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [listaAmigos[i], listaAmigos[j]] = [listaAmigos[j], listaAmigos[i]];
-   }
-   alert(listaAmigos.innerHTML)
-
+    if(amigo.value ==''){
+        alert('Informe o nome do amigo!');
+        return;
     }
 
+    if(amigos.includes(amigo.value)){
+        alert('Nome já adicionado');
+        return
+    }
 
-function reiniciar(){
-    alert('TesteA');
+    amigos.push(amigo.value);
+
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
+
+    amigo.value = '';
+}
+
+function sortear() {
+    if(amigos.length<4){
+        alert('Precisa ter pelo menos 4 amigos');
+        return
+    }
+
+    embaralhar(amigos);
+
+    let sorteio = document.getElementById('lista-sorteio');
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[0] + '<br/>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[i + 1] + '<br/>';
+        }
+    }
+}
+
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 }
